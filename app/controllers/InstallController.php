efda2009489a16a7c54f93059f489c62894291ce
Henrik Smederod <henrik@smederod.com>
2014-10-07 16:39:29 +0200

    Janitor: Remove whitespace and mixed spaces/tabs
    Remove all extra whitespace and convert all spaces indents to tabs

--- app/controllers/InstallController.php
+++ app/controllers/InstallController.php
@@ -10,7 +10,7 @@ use October\Rain\Config\Rewrite as NewConfig;
 
 class InstallController extends BaseController{
 
-	/** 
+	/**
 	*	Check the index
 	*/
 	public function getIndex()
@@ -33,12 +33,12 @@ class InstallController extends BaseController{
 		$data = Input::all();
 		$newDbConfig = new NewConfig;
 		$newDbConfig->toFile(app_path().'/config/database.php', [
-              'connections.mysql.host' =>$data['host'],
-              'connections.mysql.database' =>$data['database'],
-              'connections.mysql.username' =>$data['username'],
-              'connections.mysql.password' =>$data['password']
-            ]);
-		
+			  'connections.mysql.host' =>$data['host'],
+			  'connections.mysql.database' =>$data['database'],
+			  'connections.mysql.username' =>$data['username'],
+			  'connections.mysql.password' =>$data['password']
+			]);
+
 		DB::unprepared(file_get_contents(public_path().'/92fiveapp.sql'));
 
 		return View::make('install.timezone');
@@ -48,8 +48,8 @@ class InstallController extends BaseController{
 		$timeZone = Input::get('timezone');
 		$newAppConfig = new NewConfig;
 		$newAppConfig->toFile(app_path().'/config/app.php', [
-              'timezone'=> $timeZone 
-            ]);
+			  'timezone'=> $timeZone
+			]);
 		return View::make('install.adminaccount');
 	}
 	public function postAdminAccount()
@@ -58,38 +58,38 @@ class InstallController extends BaseController{
 		//return View::make('install.done');
 
 		try
-        {
-            $user = Sentry:: createUser(array(
-                'email'=> $data['email'],
-                'password'=>$data['password'],
-                'activated'=>true,
-                'first_name'=>$data['first_name'],
-                'last_name'=>$data['last_name'],
-                ));
-            $group = Sentry::findGroupByName('admin');
-            $user->addGroup($group);
-            $quicknote = new \Quicknote;
-            $quicknote->user_id = $user->id;
-            $quicknote->save();
-            $userProfile = new \UserProfile;
-            $userProfile->id = $user->id;
-            $userProfile->save(); 
-            $imageResult = App::make('AuthController')->{'createUserImage'}($user->id,$data['first_name'][0],$data['last_name'][0]);
-            $installationDate = date('Y-m-d H:i:s');
-            $installationHost = Request::server('PATH_INFO');
-            $new92fiveConfig = new NewConfig;
+		{
+			$user = Sentry:: createUser(array(
+				'email'=> $data['email'],
+				'password'=>$data['password'],
+				'activated'=>true,
+				'first_name'=>$data['first_name'],
+				'last_name'=>$data['last_name'],
+				));
+			$group = Sentry::findGroupByName('admin');
+			$user->addGroup($group);
+			$quicknote = new \Quicknote;
+			$quicknote->user_id = $user->id;
+			$quicknote->save();
+			$userProfile = new \UserProfile;
+			$userProfile->id = $user->id;
+			$userProfile->save();
+			$imageResult = App::make('AuthController')->{'createUserImage'}($user->id,$data['first_name'][0],$data['last_name'][0]);
+			$installationDate = date('Y-m-d H:i:s');
+			$installationHost = Request::server('PATH_INFO');
+			$new92fiveConfig = new NewConfig;
 			$new92fiveConfig->toFile(app_path().'/config/92five.php', [
-              'install'=> true,
-              'version' => '1.0',
-              'installationDate'=>$installationDate,
-              'installationHost' =>$installationHost 
-            ]);
-            return View::make('install.done');
-        }
-        catch(Exception $e)
-        {
-            Log::error('Something Went Wrong in Install Controller Repository - addUserWithDetails():'. $e->getMessage());
-            return false;
-        }  
+			  'install'=> true,
+			  'version' => '1.0',
+			  'installationDate'=>$installationDate,
+			  'installationHost' =>$installationHost
+			]);
+			return View::make('install.done');
+		}
+		catch(Exception $e)
+		{
+			Log::error('Something Went Wrong in Install Controller Repository - addUserWithDetails():'. $e->getMessage());
+			return false;
+		}
 	}
 }
