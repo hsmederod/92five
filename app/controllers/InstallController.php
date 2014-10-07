04920d5c31e440f1736468b4557205d0d0c82e11
Henrik Smederod <henrik@smederod.com>
2014-10-07 16:41:58 +0200

    Install: Create database if it doesn't exist
    Avoid the “something went wrong” exception if the database name the
    user enters doesn’t exist.
    Create the database using statement with bindings to escape user input
    value

--- app/controllers/InstallController.php
+++ app/controllers/InstallController.php
@@ -39,6 +39,10 @@ class InstallController extends BaseController{
 			  'connections.mysql.password' =>$data['password']
 			]);
 
+		// Create the database if it doesn't exist. Use bindings to escape the value
+		DB::statement('CREATE DATABASE IF NOT EXISTS :chosen_database', array('chosen_database' => $data['database']));
+
+
 		DB::unprepared(file_get_contents(public_path().'/92fiveapp.sql'));
 
 		return View::make('install.timezone');
